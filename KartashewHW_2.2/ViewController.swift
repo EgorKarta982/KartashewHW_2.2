cb9451b364aa4adc9a3444bfaf0d2af307c30329
Egor Kartshev <egorkarta982@gmail.com>
2021-10-24 22:45:06 +0300

    не получилось доделать до конца

--- KartashewHW_2.2/ViewController.swift
+++ KartashewHW_2.2/ViewController.swift
@@ -1,10 +1,6 @@
 
-// поправить округление до 1.00
-//
-//отправить хоть что то что бы проверить как работает Гитхаб
-// сделать первую домашку
-//доедалать эту даже после срока сдачи самому
-//добавить две вьюшки, закрасить их в цвета и поставить прозрачность
+// Никита, здравствуйте!
+// Я не успел доделать домашку и если честно не соображу как реальзовать, у меня получилось что то корявое. Если вы мне намекнете слегка на мои ошибки или в каком направлении двигаться, то я попробую завтра доделать. А на уроке уже посмотрю решение, спасибо!
 
 
 
@@ -12,7 +8,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var viewScreen: UIView!
+    @IBOutlet var viewScreen: UIView! // красный
+    @IBOutlet var viewScreenGreen: UIView!
+    @IBOutlet var viewScreenBlue: UIView!
+    
     
     @IBOutlet var sliderRed: UISlider!
     @IBOutlet var sliderGreen: UISlider!
@@ -25,10 +24,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        viewScreenBlue.backgroundColor = .white
         textSliderRed.text = String(sliderRed.value)
         textSliderGreen.text = String(sliderGreen.value)
         textSliderBlue.text = String(sliderBlue.value)
+        
+        // делаю изначально полную прозрачность
+        viewScreenBlue.backgroundColor = viewScreenBlue.backgroundColor?.withAlphaComponent(CGFloat(0))
+        viewScreen.backgroundColor = viewScreen.backgroundColor?.withAlphaComponent(CGFloat(0))
+        viewScreenBlue.backgroundColor = viewScreenBlue.backgroundColor?.withAlphaComponent(CGFloat(0))
 
     }
     
@@ -41,13 +45,13 @@ class ViewController: UIViewController {
     
     @IBAction func sliderGren(_ sender: Any) {
         textSliderGreen.text = String((round(sliderGreen.value * 100))/100)
-        viewScreen.backgroundColor = .green
-        viewScreen.backgroundColor = viewScreen.backgroundColor?.withAlphaComponent(CGFloat((round(sliderGreen.value * 100))/100))
+        viewScreenGreen.backgroundColor = .green
+        viewScreenGreen.backgroundColor = viewScreenGreen.backgroundColor?.withAlphaComponent(CGFloat((round(sliderGreen.value * 100))/100))
     }
     @IBAction func sliderBlue(_ sender: Any) {
-        textSliderBlue.text = String((round(sliderGreen.value * 100))/100)
-        viewScreen.backgroundColor = .blue
-        viewScreen.backgroundColor = viewScreen.backgroundColor?.withAlphaComponent(CGFloat((round(sliderBlue.value * 100))/100))
+        textSliderBlue.text = String((round(sliderBlue.value * 100))/100)
+        viewScreenBlue.backgroundColor = .blue
+        viewScreenBlue.backgroundColor = viewScreenBlue.backgroundColor?.withAlphaComponent(CGFloat((round(sliderBlue.value * 100))/100))
     }
     
 }
